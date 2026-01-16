454c71c3e44fc33505d1ce0552f791291e69f50b
Roshani Singh <2025021057@mmmut.ac.in>
2026-01-16 17:35:04 +0000

    Add variables and datatypes basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const account_Id=14455362 //const does not change after assignment otherwise it will show error//
+let accountEmail="roshanisingh2345@gmail.com"
+var accountPassword="098765"
+accountCity="Jaipur"
+
+//account_Id=2//not allowed
+
+accountEmail="hcverma7896@gmail.com"
+accountPassword="212121"
+accountCity="Mumbai"
+
+/*
+multi line comment
+*/
+
+console.log(account_Id);
+console.table([account_Id,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)//we are using node js,not browser
+
+console.log(3+3)
+console.log("Roshani")
+
+let name="Roshani"//string
+let age=18 //number
+let isLoggerIn=false //boolean
+
+//number => 2 to power 53
+//bigint
+//string => can be ' '," " or """ """
+//boolean =>true/false
+//null =>standalone value
+//undefined =>value is not assigned yet
+//symbol =>unique
+
+//object
+
+console.log(typeof "Roshani")
+console.log(typeof age)
+
